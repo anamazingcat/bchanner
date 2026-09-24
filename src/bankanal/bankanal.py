ecbb2f1c12bafb8f5a587e7617dd2f75836704aa
anamazingcat <sesivere@gmail.com>
2026-09-24 23:15:35 +0400

    Changed to async and added table

--- src/bankanal/bankanal.py
+++ src/bankanal/bankanal.py
@@ -1,7 +1,7 @@
 import discord
 import asyncio
 import logging 
-import sqlite3
+import aiosqlite
 import os
 from discord.ext import commands
 from dotenv import load_dotenv
@@ -21,18 +21,42 @@ intents = discord.Intents.default()
 bot = commands.Bot(command_prefix="!", intents = intents, help_command=None)
 
 
-db = sqlite3.connect("amazing.db")
-cursor = db.cursor()
-bot.db = db 
+#I FORGOT ASYNC EXIST 
 
+async def setup_DB():
+    bot.db = await aiosqlite.connect("amazing.db")
+    bot.db.row_factory = aiosqlite.Row 
+
+    await bot.db.execute(
+        """
+        CREATE TABLE IF NOT EXISTS esass (
+        server_id INTEGER PRIMARY KEY,
+        banned_channel_id INTEGER DEFAULT NULL
+        )
+"""
+    )
 #--------------------------------------------------------
 
+
+
+
+
+
+#--------------------------------------------------------
 
 async def main() -> None:
     await bot.start(os.getenv("BOTSS"))
 
 def run():
-    asyncio.run(main())
+    try:
+        setup_DB()
+    except:
+        logging.error("DB")
+        return
+    try:
+        asyncio.run(main())
+    except:
+        logging.info("Bot dayandi")
 
 if __name__ == "__main__":
     logging.info("--------------Basladi--------------")
